da707abeed4ae54551fd20149c87a3d5cc17d6fa
nomuyoshi <nomuyoshi0111@gmail.com>
2019-02-13 14:07:31 +0900

    apiでデータ取得

---
+++ frontend/src/components/DocList.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p v-for="doc in docList" :key="doc.id">{{ doc.title }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    docList() {
+      return this.$store.state.docs;
+    },
+  },
+  created() {
+    this.$store.dispatch('fetchDocs');
+  },
+};
+</script>
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,16 +1,29 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import httpClient from './common/httpClient';
 
 Vue.use(Vuex);
 
+console.log(httpClient.defaults.baseURL);
+
 export default new Vuex.Store({
   state: {
-
+    docs: [],
   },
   mutations: {
-
+    setDocs(state, docs) {
+      state.docs = docs;
+    },
   },
   actions: {
-
+    fetchDocs({ commit }) {
+      httpClient.get('/docs')
+        .then((response) => {
+          commit('setDocs', response.data);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
   },
 });
