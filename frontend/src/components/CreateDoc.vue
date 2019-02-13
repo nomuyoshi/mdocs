34a0774ff8c0f96fc3f9bdc793b02fd4b7028fd8
nomuyoshi <nomuyoshi0111@gmail.com>
2019-02-13 17:48:45 +0900

    エディタ作成

---
+++ frontend/src/components/CreateDoc.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <b-field>
+      <b-input v-model="title" placeholder="タイトル"></b-input>
+    </b-field>
+    <hr>
+    <div class="columns">
+      <div class="column is-half">
+        <div class="editor">
+          <b-field>
+            <b-input type="textarea" placeholder="本文" rows="20" @input="updateBody" :value="body"/>
+          </b-field>
+        </div>
+      </div>
+      <div class="column is-half">
+        <preview :compiled-html="compiledHtml"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import marked from 'marked';
+import { debounce } from 'lodash';
+import Preview from './Preview.vue';
+
+export default {
+  data() {
+    return {
+      title: '',
+      body: '',
+    };
+  },
+  computed: {
+    compiledHtml() {
+      return marked(this.body, { sanitize: true, breaks: true });
+    },
+  },
+  methods: {
+    // eslint-disable-next-line func-names
+    updateBody: debounce(function (value) {
+      this.body = value;
+    }, 300),
+  },
+  components: {
+    Preview,
+  },
+};
+</script>
