5a542cf05f982005eb927930cf6becfe5aea3ba3
nomuyoshi <nomuyoshi0111@gmail.com>
2019-02-14 23:58:49 +0900

    DBのデータを返す

--- api/views/documents.py
+++ api/views/documents.py
@@ -1,23 +1,8 @@
 import json
-from django.http import HttpResponse
-import pdb;
+from django.http import JsonResponse
+from ..models import Document
 
 def index(request):
-    data = [
-        {
-            'id': 1,
-            'title': 'djangoについて',
-        },
-        {
-            'id': 2,
-            'title': 'vueについて',
-        },
-        {
-            'id': 3,
-            'title': 'reactについて',
-        }
-    ]
+    docs = Document.objects.order_by('-created_at').values()
+    return JsonResponse({ 'docs': list(docs) })
 
-    json_str = json.dumps(data, ensure_ascii=False, indent=2)
-    response = HttpResponse(json_str, content_type='application/json; charset=UTF-8', status=None)
-    return response
