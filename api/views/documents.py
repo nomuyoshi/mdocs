e89c6155d317235a200f4202615fceb2e4f35889
nomuyoshi <nomuyoshi0111@gmail.com>
2019-02-14 20:06:42 +0900

    apiのレスポンス修正

--- api/views/documents.py
+++ api/views/documents.py
@@ -1,8 +1,9 @@
 import json
 from django.http import HttpResponse
+import pdb;
 
 def index(request):
-    docs = [
+    data = [
         {
             'id': 1,
             'title': 'djangoについて',
@@ -17,7 +18,6 @@ def index(request):
         }
     ]
 
-    data = { 'data': docs }
     json_str = json.dumps(data, ensure_ascii=False, indent=2)
     response = HttpResponse(json_str, content_type='application/json; charset=UTF-8', status=None)
     return response
