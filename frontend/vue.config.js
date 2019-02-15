f567c2ed99c90e8b7b4d28e4f791e8d7ee60c80f
nomuyoshi <nomuyoshi0111@gmail.com>
2019-02-15 22:53:01 +0900

    viewからrenderする
    fix vue.config.js

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,18 +1,15 @@
-const BundleTracker = require("webpack-bundle-tracker");
+const BundleTracker = require('webpack-bundle-tracker');
 
 module.exports = {
-  baseUrl: "http://0.0.0.0:8080/",
   outputDir: './dist/',
-  chainWebpack: config => {
-    config.optimization.splitChunks(false)
-    config.plugin('BundleTracker').use(BundleTracker, [{filename: '../frontend/webpack-stats.json'}])
-    config.resolve.alias.set('__STATIC__', 'static')
+  chainWebpack: (config) => {
+    config.optimization.splitChunks(false);
+    config.plugin('BundleTracker').use(BundleTracker, [{ filename: '../frontend/webpack-stats.json' }]);
+    config.resolve.alias.set('__STATIC__', 'static');
     config.devServer
-      .public('http://0.0.0.0:8080')
-      .host('0.0.0.0')
-      .port(8080)
+      .public('http://localhost:8080')
       .hotOnly(true)
-      .watchOptions({poll: 1000})
-      .https(false)
+      .watchOptions({ poll: 1000 })
+      .https(false);
   },
 };
