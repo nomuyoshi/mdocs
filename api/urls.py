a2c8756f8ef479a41884d8a4641f5ae183656196
nomuyoshi <nomuyoshi0111@gmail.com>
2019-02-18 12:23:58 +0900

    fix url

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,6 @@ from api import views
 
 app_name = 'api'
 urlpatterns = [
-    path('docs/', views.documents.index),
+    path('docs', views.documents.index),
     path('docs/create', views.documents.create),
 ]
