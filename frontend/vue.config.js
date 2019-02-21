27cd848dba4ba7b1853c7c9970f84ddf75cf5acd
nomuyoshi <nomuyoshi0111@gmail.com>
2019-02-21 17:26:36 +0900

    fix vue.config

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,14 +1,14 @@
 const BundleTracker = require('webpack-bundle-tracker');
 
 module.exports = {
-  publicPath: 'http://localhost:8080/',
+  publicPath: 'http://127.0.0.1:8080/',
   outputDir: './dist/',
   chainWebpack: (config) => {
     config.optimization.splitChunks(false);
     config.plugin('BundleTracker').use(BundleTracker, [{ filename: './webpack-stats.json' }]);
     config.resolve.alias.set('__STATIC__', 'static');
     config.devServer
-      .public('http://localhost:8080')
+      .public('http://127.0.0.1:8080')
       .hotOnly(true)
       .watchOptions({ poll: 1000 })
       .https(false);
