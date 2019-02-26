4db4a3492f8eefd59bfdd23457a072eea3dca376
nomuyoshi <nomuyoshi0111@gmail.com>
2019-02-26 20:49:39 +0900

    current_userで絞り込み

--- api/urls.py
+++ api/urls.py
@@ -2,4 +2,4 @@ from rest_framework import routers
 from .views import DocumentViewSet
 
 router = routers.DefaultRouter()
-router.register(r'docs', DocumentViewSet)
+router.register(r'docs', DocumentViewSet, 'documents')
