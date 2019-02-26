62eae9dc193e8e40bf2a80051a2c06dd80dc5fae
nomuyoshi <nomuyoshi0111@gmail.com>
2019-02-26 15:04:50 +0900

    current_userで絞り込み

--- api/views.py
+++ api/views.py
@@ -3,5 +3,8 @@ from .models import Document
 from .serializers import DocumentSerializer
 
 class DocumentViewSet(viewsets.ModelViewSet):
-    queryset = Document.objects.order_by('-created_at').all()
     serializer_class = DocumentSerializer
+
+    def get_queryset(self):
+        current_user = self.request.user
+        return current_user.document_set.order_by('-created_at').all()
