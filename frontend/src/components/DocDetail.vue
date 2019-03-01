206043328c51bc93997a08ae84b424109e1ac4f8
nomuyoshi <nomuyoshi0111@gmail.com>
2019-03-01 20:23:07 +0900

    削除機能追加と詳細画面APIからデータを取るように変更

--- frontend/src/components/DocDetail.vue
+++ frontend/src/components/DocDetail.vue
@@ -1,19 +1,24 @@
 <template>
   <div class="columns">
     <div class="column is-three-quarters">
-      <section class="section" v-if="doc">
+      <section class="section">
         <div class="container">
           <div class="level">
             <div class="level-left">
               <div class="level-item">
-                <p class="title">{{ doc.title }}</p>
+                <p class="title">{{ title }}</p>
+              </div>
+              <div class="level-item">
+                <router-link :to="{ name: 'doc-edit', params: { id: id } }" class="button">
+                  <b-icon pack="fas" icon="edit" type="is-info"></b-icon>
+                </router-link>
               </div>
             </div>
             <div class="level-right">
               <div class="level-item">
-                <router-link :to="{ name: 'doc-edit', params: { id: doc.id } }" class="button">
-                  <b-icon pack="fas" icon="edit" type="is-info"></b-icon>
-                </router-link>
+                <button class="button" type="button" @click="onClickDelete">
+                  <b-icon pack="fas" icon="trash-alt" type="is-danger"></b-icon>
+                </button>
               </div>
             </div>
           </div>
@@ -26,21 +31,59 @@
 </template>
 
 <script>
+import axios from 'axios';
 import marked from 'marked';
+
+import router from '../router';
 import Preview from './Preview.vue';
 
 export default {
-  computed: {
-    doc() {
-      return this.$store.getters.getDocById(this.$route.params.id);
-    },
-    compiledHtml() {
-      if (!this.doc) { return ''; }
-      return marked(this.doc.body, { sanitize: true, breaks: true });
-    },
+  data() {
+    return {
+      id: this.$route.params.id,
+      title: '',
+      body: '',
+    };
   },
   components: {
     Preview,
+  },
+  computed: {
+    compiledHtml() {
+      if (!this.body) { return ''; }
+      return marked(this.body, { sanitize: true, breaks: true });
+    },
+  },
+  mounted() {
+    axios.get(`/docs/${this.id}/`)
+      .then((response) => {
+        const { title, body } = response.data;
+        this.title = title;
+        this.body = body;
+      })
+      .catch(() => {
+        this.$notify({
+          group: 'notifications',
+          type: 'error',
+          title: 'エラー',
+          text: 'データが存在しません。',
+        });
+        router.push('/');
+      });
+  },
+  methods: {
+    onClickDelete() {
+      axios.delete(`/docs/${this.id}/`)
+        .then(() => router.push('/'))
+        .catch(() => {
+          this.$notify({
+            group: 'notifications',
+            type: 'error',
+            title: 'エラー',
+            text: '削除できませんでした。時間をおいて再度お試しください。',
+          });
+        });
+    },
   },
 };
 </script>
