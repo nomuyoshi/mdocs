b77d0e08e2490ffb07650e5312318d023cfb139f
nomuyoshi <nomuyoshi0111@gmail.com>
2019-03-04 20:46:52 +0900

    fix tag model

--- api/models.py
+++ api/models.py
@@ -1,27 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+class Tag(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
+
 class Document(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     body = models.TextField(max_length=10000)
+    tags = models.ManyToManyField(Tag)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
-
-class Tag(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=25)
-    documents = models.ManyToManyField(Document, through='Tagging')
-
-    def __str__(self):
-        return self.name
-
-class Tagging(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
-    document = models.ForeignKey(Document, on_delete=models.CASCADE)
-
