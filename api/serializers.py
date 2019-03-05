c68d84849ea7dd3f106fcafa22469968d6b25421
nomuyoshi <nomuyoshi0111@gmail.com>
2019-03-05 20:55:20 +0900

    doc作成時にtagも作成・ヒモ付

--- api/serializers.py
+++ api/serializers.py
@@ -14,3 +14,15 @@ class DocumentSerializer(serializers.ModelSerializer):
         fields = ('id', 'user', 'title', 'body', 'tags', 'created_at', 'updated_at')
         read_only_fields = ('id', 'user')
 
+    # TODO: transaction
+    def create(self, validated_data):
+        tags_data = validated_data.pop('tags')
+        doc = Document.objects.create(**validated_data)
+
+        tags = []
+        for tag_data in tags_data:
+            tag = Tag.objects.get_or_create(user=validated_data['user'], name=tag_data['name'])[0]
+            if tag: tags.append(tag)
+
+        doc.tags.set(tags)
+        return doc
