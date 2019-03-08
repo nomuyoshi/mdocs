38c0128139b81d39bf64a962351ccd1c93efaf3f
nomuyoshi <nomuyoshi0111@gmail.com>
2019-03-08 20:11:41 +0900

    見た目調整

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,49 @@
 <template>
   <div id="app">
     <notifications group="notifications" position="bottom right" />
-    <div id="nav">
-      <router-link to="/docs/new">新規作成</router-link>
-    </div>
     <div class="container">
-      <router-view/>
+      <div class="columns">
+        <div class="column is-3" v-show="sidebarVisible()">
+          <sidebar/>
+        </div>
+        <div class="column">
+          <router-view/>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
-<style lang="scss">
+<script>
+import Sidebar from './components/Sidebar.vue';
+
+const App = {
+  components: {
+    Sidebar,
+  },
+  methods: {
+    sidebarVisible() {
+      const newUrlRegex = /\/docs\/new(\/)?$/;
+      const editUrlRegex = /\/docs\/\d+\/edit(\/)?$/;
+      // 新規作成と編集ページではサイドバー非表示
+      if (newUrlRegex.test(this.$route.path) || editUrlRegex.test(this.$route.path)) {
+        return false;
+      }
+      return true;
+    },
+  },
+};
+
+export default App;
+</script>
+
+<style lang="scss" scoped>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+}
+.container {
+  margin-top: 30px;
 }
 </style>
--- frontend/src/components/DocList.vue
+++ frontend/src/components/DocList.vue
@@ -1,17 +1,15 @@
 <template>
-  <div class="columns">
-    <div class="column is-three-quarters">
-      <ul>
-        <li class="doc-item" v-for="doc in docList" :key="doc.id">
-          <section class="section">
-            <router-link :to="{ name: 'doc-detail', params: { id: doc.id } }">
-              <h1 class="title">{{ doc.title }}</h1>
-            </router-link>
-            <doc-tags :doc="doc" />
-          </section>
-        </li>
-      </ul>
-    </div>
+  <div>
+    <ul>
+      <li class="doc-item" v-for="doc in docList" :key="doc.id">
+        <section class="section">
+          <router-link :to="{ name: 'doc-detail', params: { id: doc.id } }">
+            <h1 class="title">{{ doc.title }}</h1>
+          </router-link>
+          <doc-tags :doc="doc" />
+        </section>
+      </li>
+    </ul>
   </div>
 </template>
 
