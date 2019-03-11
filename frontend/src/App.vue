a34e7825dab22e81063c786c32e8ad806f5cd97c
nomuyoshi <nomuyoshi0111@gmail.com>
2019-03-11 18:29:31 +0900

    commonに移動

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -16,8 +16,8 @@
 </template>
 
 <script>
-import Navbar from './components/Navbar.vue';
-import Sidebar from './components/Sidebar.vue';
+import Navbar from './components/common/Navbar.vue';
+import Sidebar from './components/common/Sidebar.vue';
 
 const App = {
   components: {
