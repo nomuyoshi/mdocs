385278cb408b8fa51bdcf4fe349dc93a97df3a56
nomuyoshi <nomuyoshi0111@gmail.com>
2019-03-13 14:38:05 +0900

    タグ一覧をサイドバーに表示

--- frontend/src/components/Sidebar.vue
+++ frontend/src/components/Sidebar.vue
@@ -3,5 +3,18 @@
     <router-link to="/docs/new" class="button is-link is-medium">
       <b-icon icon="edit"></b-icon><span>新規作成</span>
     </router-link>
+    <tag-list/>
   </div>
 </template>
+
+<script>
+import TagList from './TagList.vue';
+
+const Sidebar = {
+  components: {
+    TagList,
+  },
+};
+
+export default Sidebar;
+</script>
---
+++ frontend/src/components/TagList.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav class="panel">
+    <p class="panel-heading is-size-6">
+      <b-icon icon="tags" size="is-small"></b-icon>
+      タグ一覧
+    </p>
+    <a class="panel-block" v-for="tag in tags" :key="tag.id">
+      {{ tag.name }} ({{ tag.documents_count }})
+    </a>
+  </nav>
+</template>
+
+<script>
+import axios from 'axios';
+
+const TagList = {
+  data() {
+    return {
+      tags: [],
+    };
+  },
+  mounted() {
+    axios.get('/tags/')
+      .then((response) => {
+        this.tags = response.data;
+      })
+      .catch(() => {
+        console.error('error');
+      });
+  },
+};
+
+export default TagList;
+</script>
