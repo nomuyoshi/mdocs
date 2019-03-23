7837363f19c3ee6e5260a57fdbad71ce8335ea6a
nomuyoshi <nomuyoshi0111@gmail.com>
2019-03-23 11:26:14 +0900

    adsense component

---
+++ frontend/src/components/AdSense.vue
@@ -0,0 +1,40 @@
+<template>
+  <ins
+    class="adsbygoogle"
+    :data-ad-client="adClient"
+    :data-ad-slot="adSlot"
+    :data-ad-format="adFormat"
+    :style="adStyle"
+  >
+  </ins>
+</template>
+
+<script>
+const Adsense = {
+  props: {
+    adClient: {
+      type: String,
+      required: true,
+    },
+    adSlot: {
+      type: String,
+      required: true,
+    },
+    adFormat: {
+      type: String,
+      required: false,
+      default: 'auto',
+    },
+    adStyle: {
+      type: String,
+      required: false,
+      default: 'display: block',
+    },
+  },
+  mounted() {
+    (window.adsbygoogle = window.adsbygoogle || []).push({});
+  },
+};
+
+export default Adsense;
+</script>
--- frontend/src/views/DocListView.vue
+++ frontend/src/views/DocListView.vue
@@ -1,12 +1,25 @@
 <template>
   <div class="section">
-    <h2 class="is-size-5 margin-bottom20">Myドキュメント</h2>
-    <doc-list :query="query" />
+    <div class="columns">
+      <div class="column is-9">
+        <h2 class="is-size-5 margin-bottom20">Myドキュメント</h2>
+        <doc-list :query="query" />
+      </div>
+      <div class="column">
+        <ad-sense
+          ad-client="ca-pub-4852982764298703"
+          ad-slot="8639259692"
+          ad-style="display: block"
+          ad-format="auto"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import DocList from '@/components/DocList.vue';
+import AdSense from '@/components/AdSense.vue';
 
 const DocListView = {
   computed: {
@@ -19,6 +32,7 @@ const DocListView = {
   },
   components: {
     DocList,
+    AdSense,
   },
 };
 
