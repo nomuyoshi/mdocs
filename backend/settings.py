afbc7fba281140cabedd40b4055164b0e6735433
nomuyoshi <nomuyoshi0111@gmail.com>
2019-03-24 13:48:58 +0900

    ログインページにスクショ追加

--- backend/settings.py
+++ backend/settings.py
@@ -118,13 +118,13 @@ USE_TZ = True
 # static
 #########################
 # プロジェクト直下のstaticディレクトリを指定
-#STATICFILES_DIRS = (
-#    os.path.join(BASE_DIR, 'static'),
-#)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'staticfiles'),
+]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 #########################
